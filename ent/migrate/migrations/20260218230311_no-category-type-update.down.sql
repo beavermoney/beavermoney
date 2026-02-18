69d51cd73b9ab5fc4e455ff3d73bf38eede7b981
Joey Yu <joey@jyu.dev>
2026-02-18 18:03:23 -0500

    migrate
    Signed-off-by: Joey Yu <joey@jyu.dev>

---
+++ ent/migrate/migrations/20260218230311_no-category-type-update.down.sql
@@ -0,0 +1,2 @@
+-- reverse: modify "transactions" table
+ALTER TABLE "transactions" DROP CONSTRAINT "transactions_transaction_categories_transactions", ADD CONSTRAINT "transactions_transaction_categories_transactions" FOREIGN KEY ("category_id") REFERENCES "transaction_categories" ("id") ON UPDATE NO ACTION ON DELETE NO ACTION;
