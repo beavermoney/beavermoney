f55efa673d035ab8094bf07bcef5b09707f2b402
Joey Yu <joey@jyu.dev>
2026-03-12 22:53:10 -0400

    nice
    Signed-off-by: Joey Yu <joey@jyu.dev>

--- web/src/routes/_user/household/$householdId/-components/hotkeys.tsx
+++ web/src/routes/_user/household/$householdId/-components/hotkeys.tsx
@@ -1,5 +1,6 @@
 import { useHotkeySequence } from '@tanstack/react-hotkeys'
 import { useNavigate } from '@tanstack/react-router'
+import { identity } from 'lodash-es'
 
 const Hotkeys = () => {
   const navigate = useNavigate()
@@ -43,6 +44,46 @@ const Hotkeys = () => {
     navigate({
       to: '.',
       search: (prev) => ({ ...prev, log_type: 'move' }),
+    })
+  })
+
+  useHotkeySequence(['G', 'T'], () => {
+    navigate({
+      from: '/household/$householdId',
+      to: '/household/$householdId/transactions',
+      search: identity,
+    })
+  })
+
+  useHotkeySequence(['G', 'A'], () => {
+    navigate({
+      from: '/household/$householdId',
+      to: '/household/$householdId/accounts',
+      search: identity,
+    })
+  })
+
+  useHotkeySequence(['G', 'C'], () => {
+    navigate({
+      from: '/household/$householdId',
+      to: '/household/$householdId/categories',
+      search: identity,
+    })
+  })
+
+  useHotkeySequence(['G', 'I'], () => {
+    navigate({
+      from: '/household/$householdId',
+      to: '/household/$householdId/investments',
+      search: identity,
+    })
+  })
+
+  useHotkeySequence(['G', 'S'], () => {
+    navigate({
+      from: '/household/$householdId',
+      to: '/household/$householdId/subscriptions',
+      search: identity,
     })
   })
 
