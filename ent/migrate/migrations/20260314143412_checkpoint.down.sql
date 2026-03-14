69102a3a9e616a4a260e33ce7030ed8ddf808b72
Joey Yu <joey@jyu.dev>
2026-03-14 10:34:20 -0400

    checkpoint migration
    Signed-off-by: Joey Yu <joey@jyu.dev>

---
+++ ent/migrate/migrations/20260314143412_checkpoint.down.sql
@@ -0,0 +1,4 @@
+-- reverse: create index "checkpoint_household_id_create_time" to table: "checkpoints"
+DROP INDEX "checkpoint_household_id_create_time";
+-- reverse: create "checkpoints" table
+DROP TABLE "checkpoints";
