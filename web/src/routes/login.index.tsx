404678802e55824e5c058a63de172235ebbff3af
Joey Yu <joey@jyu.dev>
2026-04-04 21:50:23 -0400

    login style
    Signed-off-by: Joey Yu <joey@jyu.dev>

--- web/src/routes/login.index.tsx
+++ web/src/routes/login.index.tsx
@@ -8,14 +8,20 @@ export const Route = createFileRoute('/login/')({
 
 function RouteComponent() {
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-      <div className="flex w-full max-w-sm flex-col gap-6">
-        <Link
-          to="/"
-          className="flex w-auto flex-col items-center justify-center font-medium"
-        >
-          <Logo size={128} className="rounded-xl" />
-          <span>Beaver Money</span>
+    <div className="flex min-h-svh flex-col items-center justify-center gap-10 p-6 md:p-10">
+      <div className="flex w-full max-w-xs flex-col items-center gap-6">
+        <Link to="/" className="flex flex-col items-center gap-4 no-underline">
+          <Logo size={96} className="rounded-xl" />
+          <div className="flex flex-col items-center gap-1">
+            <h1 className="text-center text-xl font-semibold tracking-tight">
+              Log your transactions.
+              <br />
+              Build your dam.
+            </h1>
+            <p className="text-muted-foreground text-center text-sm">
+              Login with your Google account
+            </p>
+          </div>
         </Link>
         <LoginForm />
       </div>
