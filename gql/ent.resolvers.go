a639ab78e58a9b97d2e78cd3da7070dfb1ab11ae
Joey Yu <joey@jyu.dev>
2026-04-12 16:24:22 -0400

    remove
    Signed-off-by: Joey Yu <joey@jyu.dev>

--- gql/ent.resolvers.go
+++ gql/ent.resolvers.go
@@ -1072,16 +1072,3 @@ type transactionEntryWhereInputResolver struct{ *Resolver }
 type updateInvestmentLotInputResolver struct{ *Resolver }
 type updateRecurringSubscriptionInputResolver struct{ *Resolver }
 type updateTransactionEntryInputResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//    it when you're done.
-//  - You have helper methods in this file. Move them out to keep these resolver files clean.
-/*
-	func (r *Resolver) CreateHouseholdInput() CreateHouseholdInputResolver {
-	return &createHouseholdInputResolver{r}
-}
-type createHouseholdInputResolver struct{ *Resolver }
-*/
