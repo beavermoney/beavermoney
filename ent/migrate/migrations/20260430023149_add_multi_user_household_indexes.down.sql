9760846f879dcfe7a2e7cb78210cafa4147b5587
Joey Yu <joey@jyu.dev>
2026-04-29 22:32:15 -0400

    chore(codegen): run codegen + apply migration for multi-user household
    - ent + gqlgen codegen regenerated with new schema (indexes, mutations, privacy rules)
    - Atlas migration: add_multi_user_household_indexes (4 performance indexes only)
    - Relay compiler run for new mutation artifacts
    - go build ./... passes

--- ent/migrate/migrations/20260430023149_add_multi_user_household_indexes.down.sql
+++ ent/migrate/migrations/20260430023149_add_multi_user_household_indexes.down.sql
@@ -1,7 +1,3 @@
--- reverse: create index "userhousehold_user_id_household_id" to table: "user_households"
-DROP INDEX "userhousehold_user_id_household_id";
--- reverse: drop index "userhousehold_household_id_user_id" from table: "user_households"
-CREATE UNIQUE INDEX "userhousehold_household_id_user_id" ON "user_households" ("household_id", "user_id");
 -- reverse: create index "transaction_household_id_user_id_datetime" to table: "transactions"
 DROP INDEX "transaction_household_id_user_id_datetime";
 -- reverse: create index "recurringsubscription_household_id_user_id" to table: "recurring_subscriptions"
