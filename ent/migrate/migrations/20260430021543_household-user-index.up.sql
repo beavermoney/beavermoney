61c9da26f17c1d174752ee77f03cbd39e7cb8d5d
Joey Yu <joey@jyu.dev>
2026-04-29 22:32:15 -0400

    chore(codegen): run codegen + apply migration for multi-user household
    - ent + gqlgen codegen regenerated with new schema (indexes, mutations, privacy rules)
    - Atlas migration: add_multi_user_household_indexes (4 performance indexes only)
    - Relay compiler run for new mutation artifacts
    - go build ./... passes

--- ent/migrate/migrations/20260430021543_household-user-index.up.sql
+++
@@ -1,4 +0,0 @@
--- drop index "userhousehold_user_id_household_id" from table: "user_households"
-DROP INDEX "userhousehold_user_id_household_id";
--- create index "userhousehold_household_id_user_id" to table: "user_households"
-CREATE UNIQUE INDEX "userhousehold_household_id_user_id" ON "user_households" ("household_id", "user_id");
--- ent/migrate/migrations/20260430023149_add_multi_user_household_indexes.up.sql
+++ ent/migrate/migrations/20260430023149_add_multi_user_household_indexes.up.sql
@@ -6,7 +6,3 @@ CREATE INDEX "investment_household_id_user_id" ON "investments" ("household_id",
 CREATE INDEX "recurringsubscription_household_id_user_id" ON "recurring_subscriptions" ("household_id", "user_id");
 -- create index "transaction_household_id_user_id_datetime" to table: "transactions"
 CREATE INDEX "transaction_household_id_user_id_datetime" ON "transactions" ("household_id", "user_id", "datetime");
--- drop index "userhousehold_household_id_user_id" from table: "user_households"
-DROP INDEX "userhousehold_household_id_user_id";
--- create index "userhousehold_user_id_household_id" to table: "user_households"
-CREATE UNIQUE INDEX "userhousehold_user_id_household_id" ON "user_households" ("user_id", "household_id");
