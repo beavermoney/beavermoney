cbabd5add3ef16e926b703e0aeeaed26fef0e7f5
Joey Yu <joey@jyu.dev>
2026-05-04 00:10:49 -0400

    reorder form
    Signed-off-by: Joey Yu <joey@jyu.dev>

--- web/src/routes/_user/household/$householdId/transactions/-components/new-sell.tsx
+++ web/src/routes/_user/household/$householdId/transactions/-components/new-sell.tsx
@@ -259,59 +259,6 @@ export function NewSell({ fragmentRef }: NewSellProps) {
           }}
         >
           <FieldGroup>
-            {ownerOptions.length > 1 && (
-              <form.Field
-                name="ownerUserID"
-                children={(field) => {
-                  const isInvalid =
-                    field.state.meta.isTouched && !field.state.meta.isValid
-                  return (
-                    <Field data-invalid={isInvalid}>
-                      <FieldLabel htmlFor={field.name}>Owner</FieldLabel>
-                      <OwnerSelect
-                        id={field.name}
-                        name={field.name}
-                        options={ownerOptions}
-                        value={field.state.value}
-                        onValueChange={(value) => field.handleChange(value)}
-                        onBlur={field.handleBlur}
-                        ariaInvalid={isInvalid}
-                      />
-                      {isInvalid && (
-                        <FieldError errors={field.state.meta.errors} />
-                      )}
-                    </Field>
-                  )
-                }}
-              />
-            )}
-            <form.Field
-              name="description"
-              children={(field) => {
-                const isInvalid =
-                  field.state.meta.isTouched && !field.state.meta.isValid
-                return (
-                  <Field data-invalid={isInvalid}>
-                    <FieldLabel htmlFor={field.name}>Description</FieldLabel>
-                    <Input
-                      data-1p-ignore
-                      id={field.name}
-                      name={field.name}
-                      value={field.state.value}
-                      onBlur={field.handleBlur}
-                      onChange={(e) => field.handleChange(e.target.value)}
-                      aria-invalid={isInvalid}
-                      placeholder="Sell AAPL shares"
-                      autoComplete="off"
-                    />
-                    {isInvalid && (
-                      <FieldError errors={field.state.meta.errors} />
-                    )}
-                  </Field>
-                )
-              }}
-            />
-
             <form.Field
               name="accountId"
               children={(field) => {
@@ -554,6 +501,60 @@ export function NewSell({ fragmentRef }: NewSellProps) {
                 )
               }}
             />
+
+            <form.Field
+              name="description"
+              children={(field) => {
+                const isInvalid =
+                  field.state.meta.isTouched && !field.state.meta.isValid
+                return (
+                  <Field data-invalid={isInvalid}>
+                    <FieldLabel htmlFor={field.name}>Description</FieldLabel>
+                    <Input
+                      data-1p-ignore
+                      id={field.name}
+                      name={field.name}
+                      value={field.state.value}
+                      onBlur={field.handleBlur}
+                      onChange={(e) => field.handleChange(e.target.value)}
+                      aria-invalid={isInvalid}
+                      placeholder="Sell AAPL shares"
+                      autoComplete="off"
+                    />
+                    {isInvalid && (
+                      <FieldError errors={field.state.meta.errors} />
+                    )}
+                  </Field>
+                )
+              }}
+            />
+
+            {ownerOptions.length > 1 && (
+              <form.Field
+                name="ownerUserID"
+                children={(field) => {
+                  const isInvalid =
+                    field.state.meta.isTouched && !field.state.meta.isValid
+                  return (
+                    <Field data-invalid={isInvalid}>
+                      <FieldLabel htmlFor={field.name}>Owner</FieldLabel>
+                      <OwnerSelect
+                        id={field.name}
+                        name={field.name}
+                        options={ownerOptions}
+                        value={field.state.value}
+                        onValueChange={(value) => field.handleChange(value)}
+                        onBlur={field.handleBlur}
+                        ariaInvalid={isInvalid}
+                      />
+                      {isInvalid && (
+                        <FieldError errors={field.state.meta.errors} />
+                      )}
+                    </Field>
+                  )
+                }}
+              />
+            )}
 
             <form.Field
               name="datetime"
