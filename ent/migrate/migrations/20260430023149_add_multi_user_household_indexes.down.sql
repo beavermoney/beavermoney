d3468a770e711363b6f3259ce8cb4ef6e524e42c
Joey Yu <joey@jyu.dev>
2026-05-19 23:39:36 -0400

    migrate
    Signed-off-by: Joey Yu <joey@jyu.dev>

--- ent/migrate/migrations/20260430023149_add_multi_user_household_indexes.down.sql
+++
@@ -1,8 +0,0 @@
--- reverse: create index "transaction_household_id_user_id_datetime" to table: "transactions"
-DROP INDEX "transaction_household_id_user_id_datetime";
--- reverse: create index "recurringsubscription_household_id_user_id" to table: "recurring_subscriptions"
-DROP INDEX "recurringsubscription_household_id_user_id";
--- reverse: create index "investment_household_id_user_id" to table: "investments"
-DROP INDEX "investment_household_id_user_id";
--- reverse: create index "account_household_id_user_id" to table: "accounts"
-DROP INDEX "account_household_id_user_id";
--- ent/migrate/migrations/20260512212623_drop-tx-entry-currency.down.sql
+++
@@ -1,2 +0,0 @@
--- reverse: modify "transaction_entries" table
-ALTER TABLE "transaction_entries" ADD COLUMN "household_currency_id" bigint NOT NULL;
